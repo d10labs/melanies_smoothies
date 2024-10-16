7bef56ff50623d35d23653c14638386aed1e0b21
d10labs <93218205+d10labs@users.noreply.github.com>
2024-10-15 23:31:33 -0600

    Add search

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,9 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
     st.write(ingredients_string)
 
